349e61fb0e7a173239d55d557f14e1a0b0c7111a
Guillermo Jofre <jofregf@gmail.com>
2023-06-20 00:55:40 -0300

    style resto details

--- src/components/RestoDetails/RestoDetails.jsx
+++ src/components/RestoDetails/RestoDetails.jsx
@@ -1,27 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {Link, useParams} from "react-router-dom"
-import {getRestaurantDetails} from "../../redux/actions/restaurantActions"
-import Review from "../Reviews/Reviews"
-import Cookie from "universal-cookie"
-import {Button} from "react-bootstrap"
-
+import { Link, useParams } from "react-router-dom";
+import { getRestaurantDetails } from "../../redux/actions/restaurantActions";
+import Review from "../Reviews/Reviews";
+import Cookie from "universal-cookie";
+import { Button } from "react-bootstrap";
+import { TiArrowBack } from "react-icons/ti";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import { GiRuleBook } from "react-icons/gi";
+import "./RestoDetails.css";
 
 function RestoDetails() {
-
     const { id } = useParams();
     const dispatch = useDispatch();
     const [details] = useState(id);
 
     useEffect(() => {
         dispatch(getRestaurantDetails(details));
-    
     }, [dispatch, details]);
 
     const detailsState = useSelector((state) => {
         return Array.isArray(state.restaurants.restaurantDetails)
-        ? state.restaurants.restaurantDetails
-        : [state.restaurants.restaurantDetails]
+            ? state.restaurants.restaurantDetails
+            : [state.restaurants.restaurantDetails];
     });
 
     if (!detailsState) {
@@ -30,78 +31,183 @@ function RestoDetails() {
 
     const cookie = new Cookie();
     const cookieRole = cookie.get("user")?.role;
-    
-
 
     return (
-        <div>
-            { cookieRole  === "ROLE_RESTO" ? (
-                <div>
-                    <Link to="/resto">
-                        <Button variant="outline" className="custom-button">RESTO PANEL</Button>
-                    </Link>
-                </div>
-            ) : (
-                null
-            )}
-            {detailsState && detailsState.length > 0 ? (
-                detailsState.map((detail) => (
-                    <div key={detail.restaurantId}>
-                        <h2>{detail.restaurantName}</h2>
-                        <p>Dirección</p>
-                        <p>{detail.restaurantAddress}</p>
-                        <p>Teléfono</p>
-                        <p>{detail.restaurantPhone}</p>
-                        <p>e-mail</p>
-                        <p>{detail.restaurantEmail}</p>
-                        <p>Descripción</p>
-                        <p>{detail.restaurantDescription}</p>
-                        <p>Apertura</p>
-                        <p>{detail.openingHoursRestaurant}</p>
-                        <p>Cierre</p>
-                        <p>{detail.closingHoursRestaurant}</p>
-                        {detail.restaurantImages && detail.restaurantImages.length > 0 ? (
-                            detail.restaurantImages.map((img, i) => (
-                                <div key={i}>
-                                    <img
-                                        src={img}
-                                        alt={`Imagen ${i+1}`}
-                                        height = {300}
-                                        width = {300}
-                                    />
-                                </div>
-                            ))
-                        ) : (
-                            <div>No hay imagenes disponibles</div>
-                        )}
-                        
+        <>
+            <div className="principal-container">
+                {cookieRole === "ROLE_RESTO" ? (
+                    <div>
+                        <Link to="/resto">
+                            <Button variant="outline" className="custom-button">
+                                RESTO PANEL
+                            </Button>
+                        </Link>
                     </div>
-                ))
-            ) : (
-                <div>
-                <p>No hay restaurantes disponibles</p>
-                </div>
-            )}
+                ) : null}
+                {detailsState && detailsState.length > 0 ? (
+                    detailsState.map((detail) => (
+                        <>
+                            <div className="main-container">
+                                <div className="detailProduct-container">
+                                    <div className="title-category">
+                                        <Link
+                                            to="/inicio"
+                                            style={{
+                                                color: "#D8F3FF",
+                                                fontSize: "30px",
+                                            }}
+                                        >
+                                            <TiArrowBack />
+                                        </Link>
+                                    </div>
+                                    <div className="detail-one">
+                                            <div key={detail.restaurantId} className="detail-one-right">
+                                                <h2 className="detail-one-name">
+                                                    {detail.restaurantName}
+                                                </h2>
+                                                <div className="detail-two">
+                                                    <div>
+                                                        <strong>
+                                                            Dirección
+                                                        </strong>
+                                                    </div>
+                                                    <div className="description-right">
+                                                        <p className="description-top">
+                                                            {" "}
+                                                            {
+                                                                detail.restaurantAddress
+                                                            }
+                                                        </p>
+                                                    </div>
+                                                    <div>
+                                                        <strong>
+                                                            Teléfono
+                                                        </strong>
+                                                    </div>
+                                                    <div className="description-right">
+                                                        <p className="description-top">
+                                                            {" "}
+                                                            {
+                                                                detail.restaurantPhone
+                                                            }
+                                                        </p>
+                                                    </div>
+                                                    <div>
+                                                        <strong>e-mail</strong>
+                                                    </div>
+                                                    <div className="description-right">
+                                                        <p className="description-top">
+                                                            {" "}
+                                                            {
+                                                                detail.restaurantEmail
+                                                            }
+                                                        </p>
+                                                    </div>
+                                                    <div>
+                                                        <strong>
+                                                            Descripción
+                                                        </strong>
+                                                    </div>
+                                                    <div className="description-right">
+                                                        <p className="description-top">
+                                                            {" "}
+                                                            {
+                                                                detail.restaurantDescription
+                                                            }
+                                                        </p>
+                                                    </div>
+                                                    <div>
+                                                        <strong>
+                                                            Apertura
+                                                        </strong>
+                                                    </div>
+                                                    <div className="description-right">
+                                                        <p className="description-top">
+                                                            {" "}
+                                                            {
+                                                                detail.openingHoursRestaurant
+                                                            }
+                                                        </p>
+                                                    </div>
+                                                    <div>
+                                                        <strong>Cierre</strong>
+                                                    </div>
+                                                    <div className="description-right">
+                                                        <p className="description-top">
+                                                            {" "}
+                                                            {
+                                                                detail.closingHoursRestaurant
+                                                            }
+                                                        </p>
+                                                    </div>
+                                                    <div className="detail-one-buttons">
+                                                        <Link
+                                                            style={{
+                                                                color: "#F15422",
+                                                                fontSize: "20px",
+                                                                textDecoration:"none",
+                                                                width: "100px",
+                                                            }}
+                                                            to={`/menus/restaurantes/${id}`}
+                                                        >
+                                                            <MdOutlineRestaurantMenu/>
+                                                            <p className="p-link">Menú</p>
+                                                        </Link>
+                                                        <Link
+                                                            style={{
+                                                                color: "#F15422",
+                                                                fontSize: "20px",
+                                                                textDecoration:"none",
+                                                                width: "100px",
+                                                            }}
+                                                            to={`/restaurantes/${id}/reserva`}
+                                                        >
+                                                            <GiRuleBook/>
+                                                            <p className="p-link">Reservar</p>
+                                                            
+                                                        </Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                                {detail.restaurantImages &&
+                                                detail.restaurantImages.length >
+                                                    0 ? (
+                                                    detail.restaurantImages.map(
+                                                        (img, i) => (
+                                                            <div className="detail-one-left" key={i}>
+                                                                <img
+                                                                    className="detail-img"
+                                                                    src={img}
+                                                                    alt={`Imagen ${
+                                                                        i + 1
+                                                                    }`}
+                                                                />
+                                                            </div>
+                                                        )
+                                                    )
+                                                ) : (
+                                                    <div>
+                                                        No hay imagenes
+                                                        disponibles
+                                                    </div>
+                                                )}
+                                            
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    ))
+                ) : (
+                    <div>
+                        <p>No hay restaurantes disponibles</p>
+                    </div>
+                )}
 
-            <Review idResto={id}/>
-            
-            
-            <Link to={`/menus/restaurantes/${id}`}>
-                <Button variant="outline" className="custom-button">Ver menú</Button>
-            </Link>
-
-            <div>
-                <Link to="/inicio">
-                    <Button variant="outline" className="custom-button">Inicio</Button>
-                </Link>
+                <Review idResto={id} />
             </div>
-
-            <Link to={`/restaurantes/${id}/reserva`}>
-                <Button variant="outline" className="custom-button">Reservar</Button>
-            </Link>
-            
-            </div>    
+        </>
     );
 }
 export default RestoDetails;
-
